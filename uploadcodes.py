65e386002633b6eeee7f6576eb74e2928155ca7e
JackNDS <jacknds@hotmail.com>
2021-08-30 17:26:29 +1000

    sdsfa page

--- uploadcodes.py
+++ uploadcodes.py
@@ -4,10 +4,9 @@ from sys import argv
 url = 'https://www4.sidelinesports.com/xpsweb/secretlist/?action=add&value='
 
 with open(argv[-1], 'r') as f:
-        data = f.read().splitlines(True)
+    data = f.read().splitlines(True)
 lines = [line.strip() for line in data]
 for line in lines:
-     x = requests.post(url+line+"&category="+argv[-2])
-     print(x.text)
-     x.close()
-
+    x = requests.post(url+line+"&category="+argv[-2])
+    print(x.text)
+    x.close()
